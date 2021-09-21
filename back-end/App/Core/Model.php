1502433bf8fc95e90cc12cfb9d9efaf6fea85907
JONATAN VINICIUS VALDIVIA <20185473@cfp127aluno.local>
2021-09-21 10:54:27 -0300

    Correcao da conexao com o banco

--- back-end/App/Core/Model.php
+++ back-end/App/Core/Model.php
@@ -6,7 +6,7 @@ class Model {
     private static $conection;
     public static function getConn(){
         if(!isset(self::$conection)){
-            self::$conection = new \PDO('mysql;host=localhost;port=3306;dbname=King_Of_Services', 'root', 'bcd127');
+            self::$conection = new \PDO('mysql:host=localhost;port=3306;dbname=King_Of_Services', 'root', 'bcd127');
         }
         return self::$conection;
     }
